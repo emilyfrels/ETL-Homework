cf424d4eb996f6a4dd778fd6227eeb96548ad676
emilyfrels <emily.n.frels@gmail.com>
2020-09-10 19:58:01 -0500

    minor updates and added a queries.sql file for the joins but also for some additional future queries

--- Resources/queries.sql
+++ Resources/queries.sql
@@ -1,1 +1,7 @@
 --join tables
+
+SELECT players.player_id, players.birth_country, players.name_first, players.name_last, players.debut, players.final_game, hall_of_fame.year_id, hall_of_fame.inducted
+FROM players
+JOIN hall_of_fame
+ON players.player_id = hall_of_fame.player_id
+
